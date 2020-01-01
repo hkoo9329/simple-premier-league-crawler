1fef80c2a5cb305a718ab14748a3e9a35e17c461
hkoo <tyrfingr12@naver.com>
2020-01-01 18:54:01 +0900

    db 추가

--- pl_match_api.py
+++ pl_match_api.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, jsonify
+from db import PL_database
+from webCrawling import PL_match_crawler
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
-
 @app.route("/match", methods=['GET','POST'])
 def match():
-    content = request.json
-    result = {'result' : True}
-    return jsonify(result)
+    row = db.executeAll("show tables")
+    return jsonify(row)
 
 
 if __name__ =='__main__':
-    app.run('0.0.0.0', port=8080)
+    db = PL_database.Database()
+    # crawler = PL_match_crawler.PL_match_crawler()
+    # crawler.PL_match_list(2020,range(1,2))
+    # crawler.driverExit()
+    db.execute("insert into pl_matchs (match_day, match_time, left_team, right_team) "+
+                                    "values ('2019-01-01','19:00','teser1','tester2')")
+    db.commit()
+    # app.run('0.0.0.0', port=8080)
+    # app.run()
